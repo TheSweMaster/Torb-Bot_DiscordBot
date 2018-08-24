d747268934833ec59b47a91b6639d3b850dc9331
Jacob Gandemo <theswemaster@gmail.com>
2018-08-24 23:43:54 +0200

    Add AppSettingsData in code

---
+++ DiscordBotTest/AppSettingsData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordBotTest
+{
+    public static class AppSettingsData
+    {
+        public static AppSettings Get()
+        {
+            return new AppSettings()
+            {
+                Keys = new Key()
+                {
+                    BotToken = "<YourBotToken>",
+                    ApiuxKey = "<YouAPIKey>"
+                }
+            };
+        }
+    }
+}
--- DiscordBotTest/AppSettingsReader.cs
+++ DiscordBotTest/AppSettingsReader.cs
@@ -1,9 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace DiscordBotTest
 {
@@ -12,18 +10,18 @@ namespace DiscordBotTest
         private const string AppSettingsFileName = "AppSettings.json";
         public static AppSettings GetAppSettings()
         {
+#if DEBUG
             return JsonConvert.DeserializeObject<AppSettings>(SearchAndReadFile(AppSettingsFileName));
+#else
+            return AppSettingsData.Get();
+#endif
         }
 
         private static string SearchAndReadFile(string filename)
         {
             string text = "";
-            string path = "";
-#if DEBUG
-            path = @"..\..\..\";
-#else
-            path = AppDomain.CurrentDomain.BaseDirectory;
-#endif
+            string path = @"..\..\..\";
+
             var filePaths = Directory.GetFiles(path, filename, SearchOption.AllDirectories);
 
             return text = (filePaths.Count() == 1)
@@ -35,15 +33,15 @@ namespace DiscordBotTest
     public class AppSettings
     {
         [JsonProperty("Keys")]
-        public Key Keys { get; protected set; }
+        public Key Keys { get; set; }
     }
 
     public class Key
     {
         [JsonProperty("botToken")]
-        public string BotToken { get; protected set; }
+        public string BotToken { get; set; }
 
         [JsonProperty("apiuxKey")]
-        public string ApiuxKey { get; protected set; }
+        public string ApiuxKey { get; set; }
     }
 }
