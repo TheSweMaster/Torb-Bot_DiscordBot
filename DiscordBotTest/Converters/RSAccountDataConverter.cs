3dbbfaddd00be9173433ec52faa5ca10fe93c26b
Jacob Gandemo <theswemaster@gmail.com>
2019-04-02 21:24:47 +0200

    Add Json converter and file repository

---
+++ DiscordBotTest/Converters/RSAccountDataConverter.cs
@@ -0,0 +1,18 @@
+﻿using DiscordBotTest.Models;
+using Newtonsoft.Json;
+
+namespace DiscordBotTest.Converters
+{
+    public static class RSAccountDataConverter
+    {
+        public static string ToJson(RSAccountData accountData)
+        {
+            return JsonConvert.SerializeObject(accountData);
+        }
+
+        public static RSAccountData ToRSAccountData(string json)
+        {
+            return JsonConvert.DeserializeObject<RSAccountData>(json);
+        }
+    }
+}
---
+++ DiscordBotTest/Converters/RSAccountDataRepository.cs
@@ -0,0 +1,36 @@
+﻿using DiscordBotTest.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace DiscordBotTest.Converters
+{
+    public class RSAccountDataRepository
+    {
+        public static readonly string FilePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\RsAccountData.json";
+        public static readonly Encoding Encoding = Encoding.UTF8;
+
+
+        public void Save(RSAccountData rSAccountData)
+        {
+            var json = RSAccountDataConverter.ToJson(rSAccountData);
+            using (StreamWriter writer = new StreamWriter(FilePath, false, Encoding))
+            {
+                writer.Write($"{json}");
+            }
+        }
+
+        public RSAccountData Get()
+        {
+            if (!File.Exists(FilePath))
+            {
+                return new RSAccountData(new ulong[] { }, new List<SkillData>());
+            }
+
+            var json = File.ReadAllText(FilePath, Encoding);
+            return RSAccountDataConverter.ToRSAccountData(json);
+        }
+    }
+}
